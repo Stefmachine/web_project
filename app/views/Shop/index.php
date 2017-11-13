d0ccd77a690b2ca38d0ba9c753e1734af26a82d3
B00merang Project <cmedelahumada@gmail.com>
2017-11-13 11:01:42 -0500

    Spacing issues, home page & camelcase!

--- app/views/Shop/index.php
+++ app/views/Shop/index.php
@@ -7,23 +7,51 @@ if($productCount){
     /**
      * @type Product $product
      */
-    foreach ($_data["products"] as $product){ ?>
-        <div>
-            <a href="<?= GlobalHelper::pageLink("shop/product/{$product->getId()}") ?>"><?= $product->getName() ?></a>
-        </div>
-    <?php }
-    //End of every products
+	?>
+	<div class="row">
+	<?php
+	
+	// Grid Generator
+	$columns=0;
+	
+	foreach ($_data["products"] as $product) { 
+		$columns++; ?>
+		<div class="col-md-4 text-center">
+			<div class="row">
+				<div class="col-md-8 col-sm-offset-2">
+					<img class="img-thumbnail" src="<?= resource('img/products/' . $product->getPicture()) ?>"/>
+				</div>
+			</div>
+			<div class="row">
+				<a href="<?= GlobalHelper::pageLink("shop/product/{$product->getId()}") ?>"><h4><?= $product->getName() ?></h4></a>
+			</div>
+		</div>
+		
+		<?php
+		if ($columns == 3)
+		{?>
+			
+				</div>
+				<div class="row">
+			<?php
+			$columns = 0;
+		}
+	}?>
+	
+	</div>
+	
+	
+    <?php //End of every products
 
     //Pages numbers
     if($pageCount > 1){ ?>
-            <div>Pages
-        <?php for ($i = 1; $i <= $pageCount; $i++){ ?>
-            <a href="<?= GlobalHelper::pageLink("shop/index/{$i}/{$_data["tag"]}") ?>"> <?= $i ?> </a>
-        <?php } ?>
-            </div>
+        <div class="col-md-12 text-center pager">Pages
+			<?php for ($i = 1; $i <= $pageCount; $i++){ ?>
+				<a href="<?= GlobalHelper::pageLink("shop/index/{$i}/{$_data["tag"]}") ?>"> <b><?= $i ?> </b></a>
+			<?php } ?>
+        </div>
     <?php }
-    //End of pages numbers
-
+	//End of pages numbers
 }
 else { ?>
     <div>Aucun produit n'a été trouvé</div>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,11 +1,18 @@
 <div class="row">
-    <div class="col-lg-offset-0 col-md-offset-2 col-sm-offset-0 col-lg-5 col-md-8 col-sm-12 text-center">
+    <!--<div class="col-lg-offset-0 col-md-offset-2 col-sm-offset-0 col-lg-5 col-md-8 col-sm-12 text-center">-->
         <div class="row">
-            <h2>Welcome to OOZE's main page</h2>
+			<div class="col-md-12">
+				<h2>Welcome to OOZE's main page</h2>
+			</div>
         </div>
         <div class="row">
-            <img src="<?= resource("img/ad1.png");?>" alt="This is an ad for the Poutinator"/>
+			<div class="col-md-12">
+				Welcome to the best online mexican food store! Would you like to buy a hot-dog instead?
+				Visit <a href="http://lejoyeuxchenil.ca"><b>Le Joyeux Chenil</b></a> for an incredible discount on fresh puppies!
+			</div>
         </div>
+		
+		<img src="<?= resource("img/ad1.png");?>" alt="This is an ad for the Poutinator" />
     </div>
 </div>
 
